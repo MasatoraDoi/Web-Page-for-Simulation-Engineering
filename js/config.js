f5d235ac1e147e201bec883d68f663b940d7933e
Masatora Doi <m261246@hiroshima-u.ac.jp>
2026-08-05 16:14:45 +0900

    Update Apps Script URL after JSONP redeploy.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Google Apps Script のウェブアプリ URL をここに貼り付けてください
 // 例: 'https://script.google.com/macros/s/XXXXXXXX/exec'
 window.APP_CONFIG = {
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzqxuJlhsvcxJ3xKHQkce3MYxKC3wT58XQDzH5Pd73hHHbvSSYjl7nS0GTVxHyU2AB1/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby9Z-2dzRIQu-EBByriSBVcya0L1j4oFLRzYi1FVmy6BHuNfNABfh5uywnrEJWvJiMo/exec',
 };
